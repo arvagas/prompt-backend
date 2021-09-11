d21b635de684c43f8f471ac202b84f53e9407a19
arvagas <arvagas@gmail.com>
2021-09-10 20:48:49 -0400

    adjusted seed file

--- database/seeds/02_appointments.js
+++ database/seeds/02_appointments.js
@@ -6,9 +6,9 @@ exports.seed = function(knex) {
       return knex('appointments').insert([
         {title: "Meeting with John", month: 9, date: 13, year: 2021, timeStart: "10:45am", timeEnd: "11:45am", userID: 1},
         {title: "Meeting with John", month: 9, date: 20, year: 2021, timeStart: "10:45am", timeEnd: "11:45am", userID: 1},
-        {title: "Meeting with John", month: 9, date: 20, year: 2021, timeStart: "12:45pm", timeEnd: "1:45pm", userID: 2},
+        {title: "Meeting with John", month: 9, date: 27, year: 2021, timeStart: "12:45pm", timeEnd: "1:45pm", userID: 1},
         {title: "Celebration", month: 9, date: 24, year: 2021, timeStart: "4:45pm", timeEnd: "5:45pm", userID: 1},
-        {title: "Tour the place", month: 9, date: 16, year: 2021, timeStart: "1:00pm", timeEnd: "2:00pm", userID: 2},
+        {title: "Tour the place", month: 9, date: 16, year: 2021, timeStart: "1:00pm", timeEnd: "2:00pm", userID: 1},
       ]);
     });
 };
